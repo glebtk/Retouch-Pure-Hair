3c47fa1e61bd2c8f466b035879d5b5b1533f55c0
Gleb Tutik <tutikgv@gmail.com>
2023-05-03 04:40:56 +0600

    Small code fix

--- transforms.py
+++ transforms.py
@@ -3,21 +3,19 @@ from albumentations.pytorch import ToTensorV2
 
 
 class Transforms:
-    def __init__(self, image_size, mean, std):
+    def __init__(self, image_size):
         self.train_transforms = A.Compose([
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
             A.Rotate(limit=180, p=1.0),
             A.RandomScale(scale_limit=(0.5, 1.5)),
             A.RandomCrop(height=image_size, width=image_size),
-            A.Normalize(mean=mean, std=std, max_pixel_value=255.0),
             ToTensorV2()
         ],
             additional_targets={"image0": "image"}
         )
 
         self.test_transforms = A.Compose([
-            A.Normalize(mean=mean, std=std, max_pixel_value=255.0),
             A.Resize(image_size, image_size),
             ToTensorV2()
         ],
