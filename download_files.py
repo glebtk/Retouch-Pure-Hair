d7d6140c2057a427a352a4c97a5fc57bc609252e
Gleb Tutik <tutikgv@gmail.com>
2023-05-03 16:36:56 +0600

    Fix checkpoint path and arg notation

--- download_files.py
+++ download_files.py
@@ -29,7 +29,7 @@ def download_files(load_dataset, load_checkpoint):
         print("=> The dataset is loaded!")
 
     if load_checkpoint:
-        # url = "https://gitlab.com/glebtutik/crimean_plants_classification_files/-/raw/main/checkpoints/model_checkpoint.zip"
+        # url = "https://gitlab.com/glebtutik/crimean_plants_classification_files/-/raw/main/model_checkpoint.zip"
         # path = "checkpoints"
         # file_name = "model_checkpoint.zip"
         #
@@ -42,8 +42,8 @@ def download_files(load_dataset, load_checkpoint):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download and unzip dataset and/or checkpoint")
-    parser.add_argument("--load-dataset", action="store_true", help="Download and unzip dataset")
-    parser.add_argument("--load-checkpoint", action="store_true", help="Download and unzip checkpoint")
+    parser.add_argument("--load_dataset", action="store_true", help="Download and unzip dataset")
+    parser.add_argument("--load_checkpoint", action="store_true", help="Download and unzip checkpoint")
     args = parser.parse_args()
 
     download_files(args.load_dataset, args.load_checkpoint)
