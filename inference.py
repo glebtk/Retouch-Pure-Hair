aed1e37cbc6d56d63a48b903ce11a9a86da5aaad
Gleb Tutik <tutikgv@gmail.com>
2023-05-05 22:05:36 +0600

    Improved code security, added a second way to use the function

--- inference.py
+++ inference.py
@@ -1,5 +1,4 @@
 import os
-import uuid
 import argparse
 import yaml
 from PIL import Image
@@ -11,33 +10,50 @@ from transforms import Transforms
 from dataset import read_image
 
 
-def inference(args):
+def check_file_exists(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File '{file_path}' not found.")
+
+
+def inference(image_path: str, model_name="model_checkpoint.pth.tar"):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    check_file_exists("config.yaml")
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
 
-    model_checkpoint_path = os.path.join(config["checkpoint_dir"], args.model_name)
+    check_file_exists(image_path)
+    image = read_image(image_path)
+
+    model_checkpoint_path = os.path.join(config.get("checkpoint_dir", ""), model_name)
+    check_file_exists(model_checkpoint_path)
     checkpoint = load_checkpoint(model_checkpoint_path, device=device)
 
     model = checkpoint["model"]
     model.eval()
 
     transforms = Transforms(image_size=256).test_transforms
-    image = read_image(args.image_path)
 
     image_tensor = transforms(image=image)["image"].unsqueeze(0).to(device) / 255
     output = model(image_tensor)
 
     output = (output * 255).clamp(0, 255).cpu().detach().squeeze(0).permute(1, 2, 0).numpy()
+    output_image = Image.fromarray(output.astype("uint8"))
+
+    output_path = os.path.join("output", f"{os.path.splitext(os.path.basename(image_path))[0]}_result.png")
+    make_directory("output")
+    output_image.save(output_path)
 
     return output
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_path", type=str, default="image.png")
-    parser.add_argument("--model_name", type=str, default="model_checkpoint.pth.tar")
+    parser.add_argument("--image_path", type=str, required=True, help="Path to the input image.")
+    parser.add_argument("--model_name", type=str, default="model_checkpoint.pth.tar", help="Name of the model checkpoint.")
     args = parser.parse_args()
 
-    inference(args)
+    try:
+        inference(args.image_path, args.model_name)
+    except Exception as e:
+        print(f"Error: {e}")
