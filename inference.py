91d145eb33523d574eb54e47d11be7412958af60
Gleb Tutik <tutikgv@gmail.com>
2023-05-05 22:45:36 +0600

    Add return import feature

--- inference.py
+++ inference.py
@@ -15,7 +15,7 @@ def check_file_exists(file_path):
         raise FileNotFoundError(f"File '{file_path}' not found.")
 
 
-def inference(image_path: str, model_name="model_checkpoint.pth.tar"):
+def inference(image_path: str, model_name="model_checkpoint.pth.tar", return_input=False):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     check_file_exists("config.yaml")
@@ -37,6 +37,7 @@ def inference(image_path: str, model_name="model_checkpoint.pth.tar"):
     image_tensor = transforms(image=image)["image"].unsqueeze(0).to(device) / 255
     output = model(image_tensor)
 
+    input = (image_tensor * 255).clamp(0, 255).cpu().detach().squeeze(0).permute(1, 2, 0).numpy()
     output = (output * 255).clamp(0, 255).cpu().detach().squeeze(0).permute(1, 2, 0).numpy()
     output_image = Image.fromarray(output.astype("uint8"))
 
@@ -44,16 +45,20 @@ def inference(image_path: str, model_name="model_checkpoint.pth.tar"):
     make_directory("output")
     output_image.save(output_path)
 
-    return output
+    if return_input:
+        return input, output
+    else:
+        return output
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_path", type=str, required=True, help="Path to the input image.")
     parser.add_argument("--model_name", type=str, default="model_checkpoint.pth.tar", help="Name of the model checkpoint.")
+    parser.add_argument("--return_input", type=bool, default=False, help="Return input images for comparation")
     args = parser.parse_args()
 
     try:
-        inference(args.image_path, args.model_name)
+        inference(args.image_path, args.model_name, args.return_input)
     except Exception as e:
         print(f"Error: {e}")
